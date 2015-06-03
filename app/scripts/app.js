6d9686cdbc14583e3fdfc050fa01c0733253d002
Daniel Sadowski <daniel.sadowski75@gmail.com>
2015-06-04 01:27:32 +0200

    Added ui-routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,15 +8,45 @@
  *
  * Main module of the application.
  */
-angular
+var mainApp = angular
   .module('yoAngularApp', [
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
+    'ngTouch',
+	'ui.router'
+  ]);
+  
+    // mainApp.config(function($routeProvider, $stateProvider, $urlRouterProvider) {
+  mainApp.config(function($routeProvider, $stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
+	//$urlRouterProvider.when('/home', '/home/index');
+	
+	
+    $stateProvider
+        // HOME STATES  ========================================
+		        .state('/', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl'
+        })
+        .state('home', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl'
+        })
+        // ABOUT STATE =================================
+        .state('about', {
+			templateUrl: 'views/about.html',
+			controller: 'AboutCtrl'
+        })
+        // ABOUT CONTACT =================================
+        .state('contact', {
+			templateUrl: 'views/contact.html',
+			controller: 'ContactCtrl'
+        });		
+});
+/*
+  mainApp.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -34,3 +64,4 @@ angular
         redirectTo: '/'
       });
   });
+*/
