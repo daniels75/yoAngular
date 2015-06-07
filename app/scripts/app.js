00d5565e6054ef54297d4383816d962fe63fd7f6
Daniel Sadowski <daniel.sadowski75@gmail.com>
2015-06-07 18:55:57 +0200

    Basic Authtication - init

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,20 +8,37 @@
  *
  * Main module of the application.
  */
+ 
+// declare modules
+angular.module('Authentication', []);
+angular.module('Home', []); 
+ 
 angular
   .module('yoAngularApp', [
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'Authentication',
+    'Home'	
   ])
   .config(function ($routeProvider) {
     $routeProvider
+		.when('/', {
+			controller: 'HomeController',
+			templateUrl: 'views/home.html'
+		})	
+		.when('/login', {
+			controller: 'LoginController',
+			templateUrl: 'views/authentication/login.html'
+		})
+		/*
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+	  */
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
@@ -33,4 +50,24 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  
+  .run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+
+		/*
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+		
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+		
+        });
+		*/
+    }]);
---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+
+/*
+angular.module('Home')
+
+.controller('HomeController',
+    ['$scope',
+    function ($scope) {
+
+    }]);
+*/	
+	
+angular.module('yoAngularApp')
+  .controller('HomeController', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });	
